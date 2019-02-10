2c27bb7574100c80363528c65a416dab1e91c6c6
xMansour <melsayedmansour96@gmail.com>
2019-02-10 13:31:09 +0200

    Created the bottom navigation in the ChildDetailsActivity and 3 fragments Apps,Location and Stats Fragment

---
+++ Application/app/src/main/java/com/mansourappdevelopment/androidapp/kidsafe/fragments/AppsFragment.java
@@ -0,0 +1,20 @@
+package com.mansourappdevelopment.androidapp.kidsafe.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.mansourappdevelopment.androidapp.kidsafe.R;
+
+public class AppsFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_apps, container, false);
+        return view;
+    }
+}
