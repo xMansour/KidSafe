bd51869a159739f0b23126333b13f28058d7d49c
xMansour <melsayedmansour96@gmail.com>
2019-02-15 23:10:31 +0200

    NotificationChannelCreator done.

---
+++ Application/app/src/main/java/com/mansourappdevelopment/androidapp/kidsafe/utils/NotificationChannelCreator.java
@@ -0,0 +1,29 @@
+package com.mansourappdevelopment.androidapp.kidsafe.utils;
+
+import android.app.Application;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
+
+public class NotificationChannelCreator extends Application {
+    public static final String CHANNEL_ID = "com.mansourappdevelopment.androidapp.kidsafe.utils.CHANNEL_ID";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        createNotificationChannel();
+    }
+
+
+    private void createNotificationChannel() {
+        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.O) {
+            NotificationChannel serviceChannel = new NotificationChannel(
+                    CHANNEL_ID,
+                    "Service Channel",
+                    NotificationManager.IMPORTANCE_LOW);
+
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(serviceChannel);
+        }
+    }
+}
