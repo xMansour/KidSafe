1ab87646bb437d5cf8e30ea83872d31ff0c4c155
xMansour <melsayedmansour96@gmail.com>
2019-02-16 01:45:09 +0200

    Need to make the Foreground service getting the stats in real time.

--- Application/app/src/main/java/com/mansourappdevelopment/androidapp/kidsafe/services/UpdateAppStatsForegroundService.java
+++ Application/app/src/main/java/com/mansourappdevelopment/androidapp/kidsafe/services/UpdateAppStatsForegroundService.java
@@ -5,16 +5,34 @@ import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
+import android.support.annotation.NonNull;
 import android.support.v4.app.NotificationCompat;
+import android.util.Log;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.GenericTypeIndicator;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 import com.mansourappdevelopment.androidapp.kidsafe.R;
 import com.mansourappdevelopment.androidapp.kidsafe.activities.ChildSignedInActivity;
+import com.mansourappdevelopment.androidapp.kidsafe.utils.App;
+import com.mansourappdevelopment.androidapp.kidsafe.utils.User;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static com.mansourappdevelopment.androidapp.kidsafe.activities.ChildSignedInActivity.CHILD_EMAIL;
 import static com.mansourappdevelopment.androidapp.kidsafe.utils.NotificationChannelCreator.CHANNEL_ID;
 
 public class UpdateAppStatsForegroundService extends Service {
     public static final int NOTIFICATION_ID = 27;
+    public static final String TAG = "UpdateAppStatsService";
+    private FirebaseDatabase firebaseDatabase;
+    private DatabaseReference databaseReference;
+    private ArrayList<App> apps;
 
     @Override
     public void onCreate() {
@@ -25,7 +43,6 @@ public class UpdateAppStatsForegroundService extends Service {
     public int onStartCommand(Intent intent, int flags, int startId) {
         String childEmail = intent.getStringExtra(CHILD_EMAIL);
         //String notificationContent = "Monitoring device";
-
 
 
         Intent notificationIntent = new Intent(this, ChildSignedInActivity.class);
@@ -39,7 +56,11 @@ public class UpdateAppStatsForegroundService extends Service {
 
         startForeground(NOTIFICATION_ID, notification);
 
-        updateAppStats(childEmail);
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        databaseReference = firebaseDatabase.getReference("users");
+
+        getApps(childEmail);
+
         return START_REDELIVER_INTENT;
     }
 
@@ -54,7 +75,43 @@ public class UpdateAppStatsForegroundService extends Service {
     }
 
 
-    private void updateAppStats(String childEmail){
+    private void updateAppStats(String childEmail) {
 
     }
+
+    public void getApps(final String childEmail) {
+        Query query = databaseReference.child("childs").orderByChild("email").equalTo(childEmail);
+        query.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists()) {
+                    //Log.i(TAG, "onDataChange: dataSnapshot value: "+dataSnapshot.getValue());
+                    //Log.i(TAG, "onDataChange: dataSnapshot as a string: "+dataSnapshot.toString());
+                    //Log.i(TAG, "onDataChange: dataSnapshot children: " + dataSnapshot.getChildren());
+                    //Log.i(TAG, "onDataChange: dataSnapshot key: " + dataSnapshot.getKey());
+
+                    apps = new ArrayList<>();
+                    DataSnapshot nodeShot = dataSnapshot.getChildren().iterator().next();
+                    User child = nodeShot.getValue(User.class);
+                    apps = child.getApps();
+
+                    Log.i(TAG, "onDataChange: child name: "+child.getName());
+
+                    for (App app : apps) {
+                        Log.i(TAG, "onDataChange: app name: " + app.getAppName() + ", blocked: " + app.isBlocked() + "\n");
+                    }
+
+                    updateAppStats(childEmail);
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+
 }
